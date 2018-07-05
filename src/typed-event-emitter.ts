1ca206e530d3aca8a37457c73151ef7d38988a3b
elderapo <tomek@martynski.pl>
2018-07-05 20:09:17 +0200

    initial implementation

--- src/typed-event-emitter.ts
+++ src/typed-event-emitter.ts
@@ -1,4 +1,126 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+import { EventEmitter } from "events";
+import { AssertionError } from "assert";
+
+type RemoveEventListener = () => void;
+
+export class TypedEventEmitter<Events> {
+  private _eventEmitter: EventEmitter = new EventEmitter();
+  private symbolMap: Map<keyof Events, symbol> = new Map();
+
+  public addListener<Event extends keyof Events>(
+    event: Event,
+    listener: (payload: Events[Event]) => void
+  ): RemoveEventListener {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    return this.on(event, listener);
+  }
+
+  public on<Event extends keyof Events>(
+    event: Event,
+    listener: (payload: Events[Event]) => void
+  ): RemoveEventListener {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    this._eventEmitter.on(eventSymbol, listener);
+    return () => this._eventEmitter.removeListener(eventSymbol, listener);
+  }
+
+  public once<Event extends keyof Events>(
+    event: Event,
+    listener: (payload: Events[Event]) => void
+  ): RemoveEventListener {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    this._eventEmitter.once(eventSymbol, listener);
+    return () => this._eventEmitter.removeListener(eventSymbol, listener);
+  }
+
+  /* istanbul ignore next */
+  public prependListener<Event extends keyof Events>(
+    event: Event,
+    listener: (payload: Events[Event]) => void
+  ): RemoveEventListener {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    this._eventEmitter.prependListener(eventSymbol, listener);
+    return () => this._eventEmitter.removeListener(eventSymbol, listener);
+  }
+
+  /* istanbul ignore next */
+  public prependOnceListener<Event extends keyof Events>(
+    event: Event,
+    listener: (payload: Events[Event]) => void
+  ): RemoveEventListener {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    this._eventEmitter.prependOnceListener(eventSymbol, listener);
+    return () => this._eventEmitter.removeListener(eventSymbol, listener);
+  }
+
+  public removeListener<Event extends keyof Events>(
+    event: Event,
+    listener: (payload: Events[Event]) => void
+  ): void {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    this._eventEmitter.removeListener(eventSymbol, listener);
+  }
+
+  public removeAllListeners<Event extends keyof Events>(event?: Event): void {
+    if (typeof event !== "undefined") {
+      const eventSymbol = this.getOrCreateEventSymbol(event);
+      this._eventEmitter.removeAllListeners(eventSymbol);
+      return;
+    }
+
+    for (let eventName of this.eventNames()) {
+      const eventSymbol = this.getOrCreateEventSymbol(eventName);
+      this._eventEmitter.removeAllListeners(eventSymbol);
+    }
+  }
+
+  public setMaxListeners(n: number): void {
+    this._eventEmitter.setMaxListeners(n);
+  }
+
+  public getMaxListeners(): number {
+    return this._eventEmitter.getMaxListeners();
+  }
+
+  public listeners<Event extends keyof Events>(event: Event): ((payload: Events[Event]) => void)[] {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    return this.nativeListenersToTypes(this._eventEmitter.listeners(eventSymbol));
+  }
+
+  private nativeListenersToTypes<Event extends keyof Events>(
+    nativeListeners: Function[]
+  ): ((payload: Events[Event]) => void)[] {
+    return <((payload: Events[Event]) => void)[]>nativeListeners;
+  }
+
+  public emit<Event extends keyof Events>(event: Event, payload: Events[Event]): void {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    this._eventEmitter.emit(eventSymbol, payload);
+  }
+
+  public eventNames(): Array<keyof Events> {
+    return Array.from(this.symbolMap.keys());
+  }
+
+  public listenerCount<Event extends keyof Events>(event: Event): number {
+    const eventSymbol = this.getOrCreateEventSymbol(event);
+    return this._eventEmitter.listenerCount(eventSymbol);
+  }
+
+  /* istanbul ignore next */
+  private getOrCreateEventSymbol<Event extends keyof Events>(event: Event): symbol {
+    if (!this.symbolMap.has(event)) {
+      this.symbolMap.set(event, Symbol(String(event)));
+    }
+
+    const s = this.symbolMap.get(event);
+
+    if (s) {
+      return s;
+    }
+
+    throw new AssertionError({
+      message: `Couldn't create symbol for event: ${event}!`
+    });
+  }
+}
--- test/typed-event-emitter.test.ts
+++ test/typed-event-emitter.test.ts
@@ -1,14 +1,272 @@
-import DummyClass from '../src/typed-event-emitter'
+import { TypedEventEmitter } from "../src/typed-event-emitter";
+import { EventEmitter } from "events";
 
-/**
- * Dummy test
- */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
+describe("TypedEventEmitter test", () => {
+  it("addListener", done => {
+    expect.assertions(2);
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+    const enum Event {
+      SomeEvent1,
+      SomeEvent2
+    }
+
+    interface Events {
+      [Event.SomeEvent1]: string;
+      [Event.SomeEvent2]: number;
+    }
+
+    const eventEmitter = new TypedEventEmitter<Events>();
+
+    eventEmitter.addListener(Event.SomeEvent1, payload => {
+      expect(payload).toBe("some payload");
+      done();
+    });
+
+    eventEmitter.addListener(Event.SomeEvent2, payload => {
+      expect(payload).toBe(666);
+      done();
+    });
+
+    eventEmitter.emit(Event.SomeEvent1, "some payload");
+    eventEmitter.emit(Event.SomeEvent2, 666);
+  });
+
+  it("on", done => {
+    expect.assertions(2);
+
+    const enum Event {
+      SomeEvent1,
+      SomeEvent2
+    }
+
+    interface Events {
+      [Event.SomeEvent1]: string;
+      [Event.SomeEvent2]: number;
+    }
+
+    const eventEmitter = new TypedEventEmitter<Events>();
+
+    eventEmitter.on(Event.SomeEvent1, payload => {
+      expect(payload).toBe("some payload");
+      done();
+    });
+
+    eventEmitter.on(Event.SomeEvent2, payload => {
+      expect(payload).toBe(666);
+      done();
+    });
+
+    eventEmitter.emit(Event.SomeEvent1, "some payload");
+    eventEmitter.emit(Event.SomeEvent2, 666);
+  });
+
+  it("once", done => {
+    expect.assertions(1);
+
+    const enum Event {
+      SomeEvent
+    }
+
+    interface Events {
+      [Event.SomeEvent]: string;
+    }
+
+    const eventEmitter = new TypedEventEmitter<Events>();
+
+    let onceRecvCount = 0;
+    eventEmitter.once(Event.SomeEvent, payload => {
+      onceRecvCount++;
+    });
+
+    eventEmitter.on(Event.SomeEvent, payload => {
+      if (payload === "last") {
+        expect(onceRecvCount).toBe(1);
+        done();
+      }
+    });
+
+    eventEmitter.emit(Event.SomeEvent, "first");
+    eventEmitter.emit(Event.SomeEvent, "aaa");
+    eventEmitter.emit(Event.SomeEvent, "bbb");
+    eventEmitter.emit(Event.SomeEvent, "ccc");
+    eventEmitter.emit(Event.SomeEvent, "last");
+  });
+
+  it("removeListener", done => {
+    expect.assertions(1);
+
+    const enum Event {
+      SomeEvent
+    }
+
+    interface Events {
+      [Event.SomeEvent]: string;
+    }
+
+    const eventEmitter = new TypedEventEmitter<Events>();
+
+    let handlerCallsCount = 0;
+    const handler = payload => {
+      handlerCallsCount++;
+    };
+
+    const handler2 = payload => {
+      handler(payload);
+    };
+
+    const remove1 = eventEmitter.on(Event.SomeEvent, handler);
+    const remove2 = eventEmitter.once(Event.SomeEvent, handler);
+    eventEmitter.on(Event.SomeEvent, handler2);
+
+    eventEmitter.emit(Event.SomeEvent, "first");
+
+    remove1();
+    remove2();
+    eventEmitter.removeListener(Event.SomeEvent, handler2);
+
+    eventEmitter.emit(Event.SomeEvent, "last");
+
+    setImmediate(() => {
+      expect(handlerCallsCount).toBe(3);
+      done();
+    });
+  });
+
+  it("removeAllListeners", done => {
+    expect.assertions(1);
+
+    const enum Event {
+      SomeEvent
+    }
+
+    interface Events {
+      [Event.SomeEvent]: string;
+    }
+
+    const eventEmitter = new TypedEventEmitter<Events>();
+
+    let handlerCallsCount = 0;
+    const handler = payload => {
+      handlerCallsCount++;
+    };
+
+    eventEmitter.on(Event.SomeEvent, handler);
+    eventEmitter.once(Event.SomeEvent, handler);
+    eventEmitter.on(Event.SomeEvent, handler);
+
+    eventEmitter.emit(Event.SomeEvent, "payload");
+
+    eventEmitter.removeAllListeners(Event.SomeEvent);
+
+    eventEmitter.emit(Event.SomeEvent, "payload");
+
+    eventEmitter.on(Event.SomeEvent, handler);
+    eventEmitter.once(Event.SomeEvent, handler);
+    eventEmitter.on(Event.SomeEvent, handler);
+
+    eventEmitter.removeAllListeners();
+
+    eventEmitter.emit(Event.SomeEvent, "payload");
+
+    setImmediate(() => {
+      expect(handlerCallsCount).toBe(3);
+      done();
+    });
+  });
+
+  it("setMaxListeners/getMaxListeners", () => {
+    const eventEmitter = new TypedEventEmitter<{}>();
+
+    expect(eventEmitter.getMaxListeners()).toBe(EventEmitter.defaultMaxListeners);
+
+    eventEmitter.setMaxListeners(666);
+
+    expect(eventEmitter.getMaxListeners()).toBe(666);
+  });
+
+  it("listeners", () => {
+    const enum Event {
+      SomeEvent1,
+      SomeEvent2 = "SomeEvent2"
+    }
+
+    interface Events {
+      [Event.SomeEvent1]: string;
+      [Event.SomeEvent2]: number;
+    }
+
+    const eventEmitter = new TypedEventEmitter<Events>();
+
+    const handler = () => {};
+
+    eventEmitter.on(Event.SomeEvent1, handler);
+    eventEmitter.once(Event.SomeEvent1, handler);
+    eventEmitter.on(Event.SomeEvent1, handler);
+
+    eventEmitter.on(Event.SomeEvent2, handler);
+    eventEmitter.once(Event.SomeEvent2, handler);
+    eventEmitter.on(Event.SomeEvent2, handler);
+    eventEmitter.on(Event.SomeEvent2, handler);
+
+    expect(eventEmitter.listeners(Event.SomeEvent1).length).toBe(3);
+    expect(eventEmitter.listeners(Event.SomeEvent2).length).toBe(4);
+  });
+
+  it("eventNames", () => {
+    const enum Event {
+      SomeEvent1,
+      SomeEvent2 = "SomeEvent2"
+    }
+
+    interface Events {
+      [Event.SomeEvent1]: string;
+      [Event.SomeEvent2]: number;
+    }
+
+    const eventEmitter = new TypedEventEmitter<Events>();
+
+    const handler = () => {};
+
+    eventEmitter.on(Event.SomeEvent1, handler);
+    eventEmitter.once(Event.SomeEvent1, handler);
+    eventEmitter.on(Event.SomeEvent1, handler);
+
+    eventEmitter.on(Event.SomeEvent2, handler);
+    eventEmitter.once(Event.SomeEvent2, handler);
+    eventEmitter.on(Event.SomeEvent2, handler);
+    eventEmitter.on(Event.SomeEvent2, handler);
+
+    expect(eventEmitter.eventNames()).toMatchObject([Event.SomeEvent1, Event.SomeEvent2]);
+  });
+
+  it("listenerCount", () => {
+    const enum Event {
+      SomeEvent1,
+      SomeEvent2,
+      SomeEvent3
+    }
+
+    interface Events {
+      [Event.SomeEvent1]: string;
+      [Event.SomeEvent2]: number;
+      [Event.SomeEvent3]: { b: string };
+    }
+
+    const eventEmitter = new TypedEventEmitter<Events>();
+
+    const handler = () => {};
+
+    eventEmitter.addListener(Event.SomeEvent1, handler);
+    eventEmitter.once(Event.SomeEvent1, handler);
+    eventEmitter.on(Event.SomeEvent1, handler);
+
+    eventEmitter.addListener(Event.SomeEvent2, handler);
+    eventEmitter.once(Event.SomeEvent2, handler);
+    eventEmitter.on(Event.SomeEvent2, handler);
+    eventEmitter.on(Event.SomeEvent2, handler);
+
+    expect(eventEmitter.listenerCount(Event.SomeEvent1)).toBe(3);
+    expect(eventEmitter.listenerCount(Event.SomeEvent2)).toBe(4);
+    expect(eventEmitter.listenerCount(Event.SomeEvent3)).toBe(0);
+  });
+});
